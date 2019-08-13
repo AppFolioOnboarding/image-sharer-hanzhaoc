ea29bb74dfd80c5b5f3e3630aa2e14a45a01ee63
Hanzhao Chen <hanzhao.chen@appfolio.com>
2019-08-12 17:31:40 -0700

    hzc - added submit new image and link feature

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,25 @@
 class ImagesController < ApplicationController
 
     def new 
-    
     end
+
+
+    def create
+        # @image = Image.new(params.require(:image).permit(:title, :link))
+        @image = Image.new(image_params)
+        
+        @image.save
+        redirect_to @image
+    end
+
+    def show
+        @image = Image.find(params[:id])
+    end
+    private
+    
+    def image_params
+        params.require(:image).permit(:title, :link)
+    end
+    
 
 end
