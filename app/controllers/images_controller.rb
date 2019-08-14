1f8660777a93d6f7682f32165b0bed90abcafc4b
Hanzhao Chen <hanzhao.chen@appfolio.com>
2019-08-13 21:08:18 -0700

    hzc - fixed indent and other typos

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,46 +1,41 @@
 class ImagesController < ApplicationController
+  def new; end
 
-    def new 
+  def create
+    @image = Image.new(image_params)
+    if !check_if_image(@image[:link])
+      flash[:notice] = 'Post does not contain a image url'
+      render 'new'
+    else
+      flash[:notice] = 'Post successfully created'
+      @image.save
+      redirect_to @image
+    end
+  end
+
+  def index
+    @images = Image.all
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :link)
+  end
+
+  def check_if_image(link)
+    tokens = link.split('.')
+
+    if !tokens.last
+      return false
+    elsif !%w[jpeg jpg gif png].include? tokens.last
+      return false
     end
 
-
-    def create
-        # @image = Image.new(params.require(:image).permit(:title, :link))
-        @image = Image.new(image_params)
-        if !check_if_image(@image[:link])
-            flash[:notice] = 'Post does not contain a image url'
-            render 'new'
-        else
-            flash[:notice] = 'Post successfully created'
-            @image.save
-            redirect_to @image
-        end
-    end
-
-
-    def index  
-        @images = Image.all
-    end
-
-    def show
-        @image = Image.find(params[:id])
-    end
-        
-    private
-        def image_params
-            params.require(:image).permit(:title, :link)
-        end
-
-        def check_if_image(link)
-            tokens = link.split('.')
-            # last = tokens.last
-            
-            if !tokens.last
-                return false
-            elsif !['jpeg', 'jpg', 'gif', 'png'].include? tokens.last
-                return false
-            end
-            return true
-        end
-
+    true
+  end
 end
