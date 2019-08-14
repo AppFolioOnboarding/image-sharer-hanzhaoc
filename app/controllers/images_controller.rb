7d2bfc8acba1d7a01f7886f89eea09a337bb412c
Hanzhao Chen <hanzhao.chen@appfolio.com>
2019-08-13 20:51:16 -0700

    hzc - add notification for invalid link

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -7,9 +7,14 @@ class ImagesController < ApplicationController
     def create
         # @image = Image.new(params.require(:image).permit(:title, :link))
         @image = Image.new(image_params)
-
-        @image.save
-        redirect_to @image
+        if !check_if_image(@image[:link])
+            flash[:notice] = 'Post does not contain a image url'
+            render 'new'
+        else
+            flash[:notice] = 'Post successfully created'
+            @image.save
+            redirect_to @image
+        end
     end
 
 
@@ -24,6 +29,18 @@ class ImagesController < ApplicationController
     private
         def image_params
             params.require(:image).permit(:title, :link)
+        end
+
+        def check_if_image(link)
+            tokens = link.split('.')
+            # last = tokens.last
+            
+            if !tokens.last
+                return false
+            elsif !['jpeg', 'jpg', 'gif', 'png'].include? tokens.last
+                return false
+            end
+            return true
         end
 
 end
