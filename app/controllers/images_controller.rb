f1ed61a0447cfeb9d15a00740598aba2ef930392
Hanzhao Chen <hanzhao.chen@appfolio.com>
2019-08-13 17:01:16 -0700

    hzc - add listing link function

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -7,19 +7,23 @@ class ImagesController < ApplicationController
     def create
         # @image = Image.new(params.require(:image).permit(:title, :link))
         @image = Image.new(image_params)
-        
+
         @image.save
         redirect_to @image
+    end
+
+
+    def index  
+        @images = Image.all
     end
 
     def show
         @image = Image.find(params[:id])
     end
+        
     private
-    
-    def image_params
-        params.require(:image).permit(:title, :link)
-    end
-    
+        def image_params
+            params.require(:image).permit(:title, :link)
+        end
 
 end
