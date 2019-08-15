07216c629ce4777e9d95d633f7831414f477fd6f
Hanzhao Chen <hanzhao.chen@appfolio.com>
2019-08-15 15:13:09 -0700

    hzc - redirect to single image view page after submit link

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,19 @@
 class ImagesController < ApplicationController
   def new; end
+
+  def create
+    @image = Image.new(image_params)
+    @image.save
+    redirect_to @image
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  private 
+
+    def image_params
+      params.require(:image).permit(:title, :link)
+    end
 end
